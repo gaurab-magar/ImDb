d1a5b59f748f0adc442cd965172358ea920cb8b6
Your Name <gdikshan10@gmail.com>
2024-05-23 03:02:13 +0545

    servcice page updated

--- src/app/service/page.js
+++ src/app/service/page.js
@@ -9,7 +9,7 @@ const page = () => {
         <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 light:text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
         <div className="h-1 w-20 bg-blue-500 rounded"></div>
       </div>
-      <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+      <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
     </div>
     <div className="flex flex-wrap -m-4">
       <div className="xl:w-1/2 md:w-1/2 p-4">
