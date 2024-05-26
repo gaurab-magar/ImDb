8a9b99657a86ac9d3505a09e4c82a3e9d78af42f
Your Name <gdikshan10@gmail.com>
2024-05-27 01:26:37 +0545

    navbar and page margin implemented

--- src/app/(Auth)/SignIn/page.js
+++ src/app/(Auth)/SignIn/page.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='mt-40'>
-        <form className="max-w-sm mx-auto shadow-md shadow-slate-500 rounded-md p-5">
+    <div className='mt-44'>
+    <form className="max-w-sm ms:mx-auto rounded-md p-5 mx-2 shadow-md shadow-slate-500 ">
         <div className="mb-5">
             <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
             <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
