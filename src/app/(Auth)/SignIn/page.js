404acd1380c82e51524199312f22bb940ae1b371
Your Name <gdikshan10@gmail.com>
2024-05-27 01:19:38 +0545

    error got escape character corrected

--- src/app/(Auth)/SignIn/page.js
+++ src/app/(Auth)/SignIn/page.js
@@ -22,7 +22,7 @@ const page = () => {
         <button type="submit" className="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign In</button>
         <div className='w-full flex justify-center items-center'>
             <Link href='/register'>
-                <p className='text-blue-600 font-semibold hover:underline dark:text-blue-500 text-xs mt-5'>Don't have an account?  
+                <p className='text-blue-600 font-semibold hover:underline dark:text-blue-500 text-xs mt-5'>Dont have an account? 
                     <span className='text-black hover:underline dark:text-white ml-2'>Register</span>
                 </p>
             </Link>
--- src/app/(Auth)/register/page.js
+++ src/app/(Auth)/register/page.js
@@ -26,7 +26,7 @@ const page = () => {
         <button type="submit" className="text-white w-full bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
         <div className='w-full flex justify-center items-center'>
             <Link href='/SignIn'>
-                <p className='text-blue-600 font-semibold hover:underline dark:text-blue-500 text-xs mt-5'>Already have an account?  
+                <p className='text-blue-600 font-semibold hover:underline dark:text-blue-500 text-xs mt-5'>Already have an account? 
                     <span className='text-black hover:underline dark:text-white ml-2'>Sign In</span>
                 </p>
             </Link>
